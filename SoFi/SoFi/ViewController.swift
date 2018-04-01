2dfda5908b3aac53540d0c809637e9972ba2d04d
Riley Hooper <ffhooper@gmail.com>
2018-03-31 18:43:23 -0600

    Initial commit.

---
+++ SoFi/SoFi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoFi
+//
+//  Created by Riley Hooper on 3/31/18.
+//  Copyright © 2018 Riley Hooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SoFi/SoFiTests/SoFiTests.swift
@@ -0,0 +1,36 @@
+//
+//  SoFiTests.swift
+//  SoFiTests
+//
+//  Created by Riley Hooper on 3/31/18.
+//  Copyright © 2018 Riley Hooper. All rights reserved.
+//
+
+import XCTest
+@testable import SoFi
+
+class SoFiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
